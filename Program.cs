6373a97e39d153c776c46e86ad954e8b224a9f87
HugoResina <148769159+HugoResina@users.noreply.github.com>
2023-10-25 13:22:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class ex14arrays
+{
+    static void Main(string[] args)
+    {
+        const int maxarray = 20;
+
+        int[] array = new int[maxarray];
+        int num;
+
+        
+    }
+}

2a204f685f3cbdaef0ab69af170f7d5582dd40f7
HugoResina <148769159+HugoResina@users.noreply.github.com>
2024-01-17 13:50:53 +0100

    Add files via upload

---
+++ game of life.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class conwaysgameoflife
+{
+    public static void Main(string[] args)
+    {
+        int neighbourCounter = 0;
+        int[,] grit = new int[50, 50];
+
+
+    }
+    public static bool checkBox(int[,] grit , int neighbourCounter)
+    {
+        for (int i = 0; i < grit.GetLength(0); i++)
+        {
+            for (int j = 0; j < grit.GetLength(1); j++)
+            {
+                if (grit[i, j])
+                {
+
+                }
+            }
+        }
+    }
+}
